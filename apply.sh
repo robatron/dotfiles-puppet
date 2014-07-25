02dffd04c98cff9d313ea7ac61da25f1375f941d
Rob McGuire-Dale <rob.mcguiredale@gmail.com>
2014-07-25 12:28:51 -0700

    Add module path to facter

--- apply.sh
+++ apply.sh
@@ -3,6 +3,7 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export FACTER_HOME="$HOME"
-export FACTER_DOTFILES="$HOME/.dotfiles"
+export FACTER_DOTFILES="$DIR"
+export FACTER_MODULE_PATH="$DIR/modules"
 
 puppet apply --modulepath=$DIR/modules $DIR/manifests/default.pp
